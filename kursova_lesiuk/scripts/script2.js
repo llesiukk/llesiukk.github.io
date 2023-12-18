4ccbac2b562e6f24fd0a0b87f5ba6b31e9f6d0ce
llesiukk <126589335+llesiukk@users.noreply.github.com>
2023-12-18 20:49:54 +0200

    Add files via upload

---
+++ kursova_lesiuk/scripts/script2.js
@@ -0,0 +1,15 @@
+MicroModal.init({
+    onShow: modal => console.info(`${modal.id} is shown`), // [1]
+    onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+    openTrigger: 'data-custom-open', // [3]
+    closeTrigger: 'data-custom-close', // [4]
+    disableScroll: true, // [5]
+    disableFocus: false, // [6]
+    awaitCloseAnimation: false, // [7]
+    debugMode: true // [8]
+  });
+  
+  var button = document.querySelector('.myButton');
+  button.addEventListener('click', function(){
+  MicroModal.show('modal-1');    
+  });
